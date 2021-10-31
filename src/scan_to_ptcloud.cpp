5a771a5c2e1f76836ac14198416060ff07278055
Kyungmin Han <thezest76@gmail.com>
2021-10-31 16:44:28 +0900

    init commit

---
+++ src/scan_to_ptcloud.cpp
@@ -0,0 +1,101 @@
+/*
+ * scan_to_ptcloud.cpp
+ *
+ *  Created on: Oct 31, 2021
+ *      Author: hankm
+ */
+
+//#ifndef SRC_SCAN_TO_PTCLOUD_CPP_
+//#define SRC_SCAN_TO_PTCLOUD_CPP_
+
+#include <scan2ptcloud/scan_to_ptcloud.hpp>
+
+namespace scan2cloud
+{
+
+//Scan2PointCloud::Scan2PointCloud(std::ofstream* poutofs, std::ofstream* pofsinfo):
+//mp_outfilestream(poutofs),
+//mp_ofsinfo(pofsinfo),
+//m_nScanCnt(0)
+//{
+//	//m_tfListener = TransformListener(m_tfBuffer);
+//
+//	m_ptCloudPub		= m_nh.advertise<sensor_msgs::PointCloud2>("scan_ptcloud",1);
+//	m_laserScanSub  	= m_nh.subscribe("base_scan", 1, &Scan2PointCloud::scanCallback, this); // kmHan
+//};
+
+Scan2PointCloud::Scan2PointCloud(const std::string& strfile_ofs, const std::string& strinfo_ofs):
+m_nScanCnt(0)
+{
+	//m_tfListener = TransformListener(m_tfBuffer);
+
+	m_outfilestream.open(strfile_ofs);
+	m_ofsinfo.open(strinfo_ofs) ;
+
+	m_ptCloudPub		= m_nh.advertise<sensor_msgs::PointCloud2>("scan_ptcloud",1);
+	m_laserScanSub  	= m_nh.subscribe("base_scan", 1, &Scan2PointCloud::scanCallback, this); // kmHan
+};
+
+
+Scan2PointCloud::~Scan2PointCloud()
+{
+	//m_outfilestream.close();
+};
+
+
+void Scan2PointCloud::scanCallback( const sensor_msgs::LaserScan::ConstPtr& scan_in )
+{
+
+	if(!m_tfListener.waitForTransform(
+		scan_in->header.frame_id,
+		"/base_link",
+		scan_in->header.stamp + ros::Duration().fromSec(scan_in->ranges.size()*scan_in->time_increment),
+		ros::Duration(1.0))){
+	 return;
+	}
+
+	tf::StampedTransform transform;
+    try{
+    	m_tfListener.lookupTransform("/map", "/laser_link",
+                               ros::Time(0), transform);
+    }
+    catch (tf::TransformException ex){
+      //ROS_ERROR("%s",ex.what());
+      //ros::Duration(1.0).sleep();
+      return;
+    }
+
+	sensor_msgs::PointCloud cloud;
+	m_projector.transformLaserScanToPointCloud("/base_link",*scan_in, cloud, m_tfListener);
+
+	// sensor pose
+	tf::Matrix3x3 Rm( transform.getRotation() ) ;
+	double roll, pit, yaw ;
+	Rm.getRPY(roll, pit, yaw);
+	m_outfilestream << "NODE " << transform.getOrigin().x() << " " << transform.getOrigin().y() << " "
+					<< transform.getOrigin().z() << " "
+					<< roll << " " << pit << " " << yaw << std::endl;
+
+	for( size_t idx=0; idx < cloud.points.size(); idx++)
+	{
+		m_outfilestream << cloud.points[idx].x << " "
+						<< cloud.points[idx].y << " "
+						<< cloud.points[idx].z << endl;
+	}
+
+	m_ofsinfo << m_nScanCnt << " " << cloud.points.size() << endl;
+
+	m_nScanCnt++;
+
+	sensor_msgs::PointCloud2 cloud2;
+	sensor_msgs::convertPointCloudToPointCloud2(cloud, cloud2);
+	m_ptCloudPub.publish(cloud2);
+
+  // Do something with cloud.
+};
+
+
+
+}
+
+//#endif /* SRC_SCAN_TO_PTCLOUD_CPP_ */
---
+++ src/scan_to_ptcloud_node.cpp
@@ -0,0 +1,61 @@
+/*
+ * scan_to_ptcloud_node.cpp
+ *
+ *  Created on: Oct 31, 2021
+ *      Author: hankm
+ */
+
+
+#include <ros/ros.h>
+#include <ros/console.h>
+
+#include "scan2ptcloud/scan_to_ptcloud.hpp"
+
+using namespace scan2cloud;
+
+int main(int argc, char** argv)
+{
+  ros::init(argc, argv, "scan2ptcloud");
+  const ros::NodeHandle nh;
+  const ros::NodeHandle private_nh("~");
+
+  ros::WallTime start_, end_;
+  if(0) //argc !=3)
+  {
+	ROS_ERROR("usage: %s <filepath>\n", argv[0]);
+	exit(-1);
+  }
+
+  ROS_INFO("args: %s %s \n", argv[0], argv[1]);
+
+  char coutfilepath[100], cinfofilepath[100];
+  sprintf(coutfilepath, "%s/scan_msg.txt", argv[1] );
+  sprintf(cinfofilepath, "%s/scan_info.txt", argv[1] );
+
+  std::string stroutfile( coutfilepath ) ;
+  std::string strinfofile( cinfofilepath ) ;
+//  ofstream ofs_out( string(outfilepath) );
+//  ofstream ofs_info( string(infofilepath) );
+//  Scan2PointCloud oScan2PtCloud( &ofs_out, &ofs_info  );
+
+  Scan2PointCloud oScan2PtCloud( stroutfile, strinfofile  );
+
+  while( ros::ok() )
+  {
+	  try{
+		  ros::spinOnce();
+	  }
+	  catch(std::runtime_error& e)
+	  {
+		ROS_ERROR("scan 2 ptcloud exception: %s", e.what());
+		return -1;
+	  }
+  }
+
+  ROS_WARN(" Shutting down scan2ptcloud process \n");
+  ros::shutdown();
+
+  return 0;
+}
+
+
